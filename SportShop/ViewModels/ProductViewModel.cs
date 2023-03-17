c21f0184173168218c5c64ab9a6626f44c50b840
Suat Alikoch <suat.alikoch@gmail.com>
2023-03-17 20:54:27 +0200

    + Added product page

---
+++ SportShop/ViewModels/ProductViewModel.cs
@@ -0,0 +1,21 @@
+﻿using SportShop.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportShop.ViewModels
+{
+    class ProductViewModel : BaseViewModel
+    {
+        public string Testak { get; set; }
+        public string Test { get; set; }
+
+        public ProductViewModel(string testak, string test)
+        {
+            Testak = testak;
+            Test = test;
+        }
+    }
+}
